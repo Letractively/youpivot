12fb0b3e20c1a6585e224e05ea07f20dab3349b7
mauricelam <mauricelam@gmail.com>
2011-02-21 01:05:26 +0000

    Second part of the previous commit

--- chromeextension/background.js
+++ chromeextension/background.js
@@ -3,10 +3,6 @@ $(function(){
 	chrome.extension.onRequest.addListener(function(request, sender, sendResponse){
 		switch(request.action){
 			case "getCurrent":
-				var id = Monitor.getSelectedId();
-				if(!id){ console.log("cannot get current id"); break; }
-				var obj = Monitor.getTabById(id);
-				sendResponse(obj);
 				break;
 			default:
 				console.log("Unknown internal request received");
