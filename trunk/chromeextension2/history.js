830739ac39dff65245bf2c19788bd0af1f72c36a
mauricelam <mauricelam@2cf73479-4798-1822-ddb6-ce0dd798f383>
2011-10-10 16:28:27 +0000

    commit extension v2 using processing.js

---
+++ trunk/chromeextension2/history.js
@@ -0,0 +1,8 @@
+$(function(){
+    // the order determines the order of the tabs on the left
+    include("youpivot.js");
+    include("traditional.js");
+    include("timemarks.js");
+
+    Master.changeTab("youpivot");
+});
