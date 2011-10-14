3177b31fe69dc91421f191fb194ff398b4be5a4b
mauricelam <mauricelam@2cf73479-4798-1822-ddb6-ce0dd798f383>
2011-10-14 15:51:44 +0000

    delete old version of extension

--- chromeextension2/history.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-    // the order determines the order of the tabs on the left
-    include("youpivot.js");
-    include("traditional.js");
-    include("timemarks.js");
-
-    Master.changeTab("youpivot");
-});
