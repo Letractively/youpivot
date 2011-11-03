a310e1cb49445be53e4c7614ce9fc840ed146d4f
mauricelam <mauricelam@2cf73479-4798-1822-ddb6-ce0dd798f383>
2011-11-03 04:25:32 +0000

    Extension to push people to new one

--- trunk/chromeextension/history.js
+++
@@ -1,9 +0,0 @@
-//document.ready is too early. CSS width is not calculated yet
-$(window).load(function(){
-	if($(window).width()==0){
-		//to fix that weird CSS bug in webkit where the width is occasionally rendered wrongly at first
-		setTimeout("GraphManager.draw()", 100);
-	}else{
-		GraphManager.draw();
-	}
-});
