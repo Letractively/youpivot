ff62ae6fc85e6c0d45dc2fe88a7c9c9635cfed71
Reedlabotz <Reedlabotz@gmail.com>
2012-02-10 22:57:12 +0000

    filter timemarks

--- chromeextension2/timemarks.js
+++ chromeextension2/timemarks.js
@@ -23,6 +23,10 @@ var TimeMarks = {};
         Master.addTab("timemarks", m, "TimeMarks", "timemarks/view.html#timemarks");
         var bar = Master.createSidebar("timemarks");
         bar.attr("id", "t-leftSidebar");
+        bar.append('<div class="sidebarGroup"><div class="sidebarLabel">Content</div><div id="tm-contentFilters" class="collapse"></div></div>');
+        include("js/views/collapsable.js");
+        var contentHandle = $("#tm-contentFilters").parent().find(".sidebarLabel");
+        $("#tm-contentFilters").collapsable("create", {handle: contentHandle, indicator: true});
     }
 })();
 
